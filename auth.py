96c98b13a4ffeb181f7bcc803eb3133701648e14
Julia Stephens <stephens.julia.h@gmail.com>
2026-08-19 19:31:25 -0700

    add JWT token creation

--- auth.py
+++ auth.py
@@ -1,4 +1,14 @@
+import os
+from datetime import datetime, timedelta, timezone
+from dotenv import load_dotenv
+from jose import jwt
 from passlib.context import CryptContext
+
+load_dotenv()
+
+SECRET_KEY = os.environ["SECRET_KEY"]
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -7,3 +17,9 @@ def hash_password(password: str) -> str:
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
